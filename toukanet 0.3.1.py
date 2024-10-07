db151d18fb5cdf7a92b848f459075e0f1a034657
Silverwolf125 <vintm2007@gmail.com>
2024-10-07 09:17:25 -0300

    Update toukanet 0.3.1.py

--- toukanet 0.3.1.py
+++ toukanet 0.3.1.py
@@ -21,18 +21,19 @@ class Player:
 
     def update(self):
 #collision platform and sprite
-        
+        #platform y = 102
         if self.x >= 0 and self.x <= 16 * TILE and self.y <= 15 * TILE and self.y >= 102:
             self.y = 102
         if self.x <= 512 and self.x >= 384 and self.y <= 15 * TILE and self.y >= 102:
             self.y = 102
-        if self.x >= 23 * TILE and self.x <= 39 * TILE and self.y <= 15* TILE and self.y >= 102:
+        if self.x >= 23 * TILE and self.x <= 40 * TILE and self.y <= 15 * TILE and self.y >= 102:
             self.y = 102
+        #platform y = 230
         if self.x >= 0 and self.x <= 16 * TILE and self.y <= 31 * TILE and self.y >= 230:
             self.y = 230
         if self.x <= 512 and self.x >= 384 and self.y <= 31 * TILE and self.y >= 230:
             self.y = 230
-        if self.x >= 23 * TILE and self.x <= 39 * TILE and self.y <= 31 * TILE and self.y >= 230:
+        if self.x >= 23 * TILE and self.x <= 40 * TILE and self.y <= 31 * TILE and self.y >= 230:
             self.y = 230
 #platform y = -136        
         self.flying = False
@@ -68,8 +69,6 @@ class Player:
         else:
             pyxel.blt(self.x, self.y, 0, 0, 0, self.direction * 16, 16, 7)
 
-
-
 class App:
     
     def __init__(self):
@@ -83,17 +82,16 @@ class App:
         self.fruits_list = []
         
         pyxel.run(self.update, self.draw)
-        
+
+#enemy create 0.5s per 1 (frame_count % 30 == 0 >>> 1s per 1)         
     def enemy_create(self):
-        
         if pyxel.frame_count % 15 == 0:
             self.enemies_list.append([random.randint (0, TILE * MAP_WIDTH - 16), 0])
-    
+            
     def fruit_create(self):
-        
         if pyxel.frame_count % 60 == 0:
             self.fruits_list.append([random.randint (0, TILE * MAP_WIDTH - 16), 0])
-            
+#gravity of bottle y + 3 per second, remove out the screen
     def enemy_move(self):
         for enemy in self.enemies_list:
             enemy[1] += 3
@@ -103,23 +101,24 @@ class App:
     def fruit_move(self):
         for fruit in self.fruits_list:
             fruit[1] += 3
-            if fruit[1] > TILE * MAP_WIDTH:
+            if fruit[1] > TILE * MAP_HEIGHT:
                 self.fruits_list.remove(fruit)
-    
+                
+#detecte collision with coordination of two sprites
     def enemy_collision(self):
-        
         for enemy in self.enemies_list:
             if enemy[0] <= self.player.x + 16 and enemy[1] <= self.player.y + 16 and enemy[0] + 8 >= self.player.x and enemy[1] + 16 >= self.player.y :
                 self.enemies_list.remove(enemy)
                 self.life -= 1
                 
     def fruit_collision(self):
-        
         for fruit in self.fruits_list:
-            if fruit[0] <= self.player.x + 16 and fruit[1] <= self.player.y + 16 and enemy[0] + 16 >= self.player.x and fruit[1] + 16 >= self.player.y:
+            if fruit[0] <= self.player.x + 16 and fruit[1] <= self.player.y + 16 and fruit[0] + 8 >= self.player.x and fruit[1] + 16 >= self.player.y :
                 self.fruits_list.remove(fruit)
                 
+                
     def update(self):
+#call function
         self.player.update()
         self.enemy_create()
         self.enemy_move()
@@ -127,12 +126,14 @@ class App:
         self.fruit_create()
         self.fruit_move()
         self.fruit_collision()
+
         
     def draw(self):
         pyxel.cls(11)
         if self.life > 0 :
             pyxel.bltm(0, 0, 0, 0, 0, MAP_WIDTH * TILE, MAP_HEIGHT * TILE)
             self.player.draw()
+#animation of heart with life leftover
             if self.life >= 1 :
                 pyxel.blt(0, 0, 0, 0, 16, 16, 16, 0)
             else:
@@ -146,19 +147,28 @@ class App:
             else:
                 pyxel.blt(32, 0, 0, 16, 16, 16, 16, 0)
             
-            
+#for enemy created(in the list) draw the sprite 
+# x, y, img, u, v, width, height, transparent color
             for enemy in self.enemies_list:
                 pyxel.blt(enemy[0], enemy[1], 0, 32, 0, 8, 16, 0)
                 
             for fruit in self.fruits_list:
                 pyxel.blt(fruit[0], fruit[1], 0, 32, 16, 16, 16, 0)
-            
+                
+#Gameover bloc     
         else:
             pyxel.bltm(0, 0, 7, 0, 0, MAP_WIDTH * TILE, MAP_HEIGHT * TILE)
             pyxel.quit
         
         
 App()
+
+
+        
+
+
+
+        
 
 
         
